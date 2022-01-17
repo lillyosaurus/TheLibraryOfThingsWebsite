ec34ed86bc802b0349fe5d5fd2660bb0e4d876eb
lillyosaurus <96991726+lillyosaurus@users.noreply.github.com>
2022-01-16 22:23:12 -0500

    remove jquery

--- scripts/helloWorld.js
+++ scripts/helloWorld.js
@@ -2,6 +2,4 @@ alert("Hello World this is an alert");
 const heading = document.querySelector('h1');
 heading.textContent = "I changed this text temporarily";
 document.write("<h2>I added this heading programatically<h2>");
-$(document).ready(function(){
-    $("#div1").append('<h2>I added this perminately</h2>');
 });
