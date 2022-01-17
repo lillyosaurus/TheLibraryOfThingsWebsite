ea0945f45d6676029aae39d765cd35f070065b41
lillyosaurus <96991726+lillyosaurus@users.noreply.github.com>
2022-01-16 20:47:04 -0500

    try adding elements programatically

--- scripts/helloWorld.js
+++ scripts/helloWorld.js
@@ -1,3 +1,4 @@
 alert("Hello World this is an alert");
 const heading = document.querySelector('h1');
-heading.textContent = "I changed this text";
+heading.textContent = "I changed this text temporarily";
+document.write("<h2>I added this heading programatically<h2>");
